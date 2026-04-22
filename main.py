3425313ab466a85ca803b73b5359294affe8ff7e
Younus <younusmashoor@gmail.com>
2026-04-22 15:41:18 +0200

    chore: cleaned codebase a bit

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
+from agno.run.agent import RunOutput
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
 
-from agno.run.agent import RunOutput
 from agent import Agno_Agent
-from agno.tools.hackernews import HackerNewsTools
 
 if __name__ == "__main__":
     agent: Agno_Agent = Agno_Agent(model_id="gemini-2.5-flash")    
-    #  Testing adding a tool
-    agent.add_tool(HackerNewsTools())
     
     # run the agent with a prompt
     agent_response: RunOutput = agent.ask("What is the top story on HackerNews?")
