42658f2a186a013ecf3c25f4cd1290ea2c0b030b
Younus <younusmashoor@gmail.com>
2026-04-29 18:04:59 +0200

    chore: cleande strcuture

--- translated_rust_code/output.rs
+++ translated_rust_code/output.rs
@@ -1,4 +1,3 @@
-```rust
 use std::io;
 
 fn two_sum(x: i32, y: i32) -> i32 {
@@ -11,18 +10,10 @@ fn main() {
 
     println!("What two numbers would you like to add?");
     println!("Number 1: ");
-    io::stdin()
-        .read_line(&mut x.to_string())
-        .expect("Failed to read line");
-
+    io::stdin().read_line(&mut x.as_mut()).expect("Failed to read line");
     println!("Number 2: ");
-    io::stdin()
-        .read_line(&mut y.to_string())
-        .expect("Failed to read line");
-
-    println!();
+    io::stdin().read_line(&mut y.as_mut()).expect("Failed to read line");
 
     let result = two_sum(x, y);
     println!("The Result: {}", result);
 }
-```
