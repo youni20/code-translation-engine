9ae6929ae6e243db34b80a31794943cd22340c38
ekrlstd <ebbekarlstad@proton.me>
2026-05-14 18:23:01 +0200

    change structure a bit

--- outputs/rust_workspace/src/output.rs
+++
@@ -1,1 +0,0 @@
-You exceeded your current quota, please check your plan and billing details. For more information on this error, read the docs: https://platform.openai.com/docs/guides/error-codes/api-errors.
