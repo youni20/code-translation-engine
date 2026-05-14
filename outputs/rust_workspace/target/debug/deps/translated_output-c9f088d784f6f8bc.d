05b969dfbada4cab485386cf46673a86ece9cf29
Younus <younusmashoor@gmail.com>
2026-05-14 22:28:21 +0200

    feat: recorded results for a 2d renderer project recived optmial and useful results

---
+++ outputs/rust_workspace/target/debug/deps/translated_output-c9f088d784f6f8bc.d
@@ -0,0 +1,5 @@
+/home/youni/Documents/work/Thesis/code-translation-engine/outputs/rust_workspace/target/debug/deps/translated_output-c9f088d784f6f8bc.d: src/output.rs
+
+/home/youni/Documents/work/Thesis/code-translation-engine/outputs/rust_workspace/target/debug/deps/libtranslated_output-c9f088d784f6f8bc.rmeta: src/output.rs
+
+src/output.rs:
